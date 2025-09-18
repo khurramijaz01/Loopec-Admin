c823bdf590073c378a3369101e6d33c75c989f4b
Khurram Ijaz <khurramijaz2016@gmail.com>
2025-09-18 09:20:04 +0500

    update

--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,51 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: "autoUpdate",
+      includeAssets: ["favicon.ico", "apple-touch-icon.png"],
+      manifest: {
+        name: "My React Vite PWA",
+        short_name: "MyPWA",
+        description: "A React app with Vite + PWA support",
+        theme_color: "#ffffff",
+        icons: [
+          {
+            src: "pwa-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "pwa-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+          {
+            src: "pwa-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "any maskable",
+          },
+        ],
+      },
+    }),
+  ],
   server: {
     proxy: {
-      '/api': {
-        target: 'https://hcy087s8e6n.sn.mynetname.net:5002',
+      "/api": {
+        target: "https://hcy087s8e6n.sn.mynetname.net:5002",
         changeOrigin: true,
         secure: false,
       },
-      '/sinaco': {
-        target: 'https://hcy087s8e6n.sn.mynetname.net:5001',
+      "/sinaco": {
+        target: "https://hcy087s8e6n.sn.mynetname.net:5001",
         changeOrigin: true,
         secure: false,
-      }
+      },
     },
   },
-})
+});
